54a5f765645d5875a17ed40abdb899e23ea12a88
KenBarr <ken.barr@solace.com>
2020-08-21 17:39:59 -0400

    Add curl rest command

--- README.md
+++ README.md
@@ -1,9 +1,9 @@
 
 Hit the "Deploy to Azure" button, and in the deployment template add the link to cp-all-in-one to Azure. 
 
-<a href="https://portal.azure.com/#create/Microsoft.Template/uri/https%3A%2F%2Fraw.githubusercontent.com%2FKenBarr%2Fpubsubplus-azure-quickstart-template%2Fmaster%2Fazuredeploy.json" target="_blank">
+<a href="https://portal.azure.com/#create/Microsoft.Template/uri/https%3A%2F%2Fraw.githubusercontent.com%2FKenBarr%2Fcp-azure-quickstart-template%2Fmaster%2Fazuredeploy.json" target="_blank">
     <img src="http://azuredeploy.net/deploybutton.png"/>
 </a>
-<a href="http://armviz.io/#/?load=https%3A%2F%2Fraw.githubusercontent.com%2FKenBarr%2Fpubsubplus-azure-quickstart-template%2Fmaster%2Fazuredeploy.json" target="_blank">
+<a href="http://armviz.io/#/?load=https%3A%2F%2Fraw.githubusercontent.com%2FKenBarr%2Fcp-azure-quickstart-template%2Fmaster%2Fazuredeploy.json" target="_blank">
     <img src="http://armviz.io/visualizebutton.png"/>
 </a>
--- scripts/populate_kafka.sh
+++ scripts/populate_kafka.sh
@@ -37,9 +37,27 @@ TOPICS=`jq < ${discovery_file} .data.events.topics[] | sed 's/"//g'`
 for topic in $TOPICS
 do
  docker exec -t broker /usr/bin/kafka-topics --bootstrap-server localhost:9092 --create --topic ${topic}
- docker exec -t broker /usr/bin/kafka-producer-perf-test --producer-props bootstrap.servers=localhost:9092 --topic ${topic} --num-records 100 --throughput -1 --record-size 100   
+ #docker exec -t broker /usr/bin/kafka-producer-perf-test --producer-props bootstrap.servers=localhost:9092 --topic ${topic} --num-records 100 --throughput -1 --record-size 100   
 done
 
+count=0
+run=1
+while [ $run -eq 1 ]
+do
+  schemaId=`jq < ${discovery_file} .data.events.topicToSchemaAssociations[${count}].schemaId`
+  topic=`jq < ${discovery_file} .data.events.topicToSchemaAssociations[${count}].topic | sed 's/"//g'`
+  echo "Schema to Topic binding ${count}:  schemaId = ${schemaId} Topic = ${topic}"
+  if [ "$schemaId" == "null" ]; then
+    break
+  fi
+  schema=`jq ".data.events.schemas[] | select(.schemaId==${schemaId})" < result.json`
+  schema_content=`echo ${schema} | jq .content`
+  schema_name=`echo ${schema} | jq .name`
+  curl -X POST -H "Content-Type: application/vnd.schemaregistry.v1+json" \
+   --data "{\"schema\": ${schema_content}}" \
+   http://localhost:8081/subjects/${topic}-value/versions
+   ((count++))
+done
 
 count=0
 run=1
@@ -51,6 +69,6 @@ do
   if [ "$consumerGroup" == "null" ]; then
     break
   fi
-   docker exec -t broker /usr/bin/kafka-consumer-perf-test --bootstrap-server localhost:9092 --topic ${topic} --group ${consumerGroup} --messages 100
+   docker exec -t broker /usr/bin/kafka-consumer-perf-test --bootstrap-server localhost:9092 --topic ${topic} --group ${consumerGroup} --messages 1 --timeout 10
   ((count++))
 done
